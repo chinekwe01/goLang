a053db107fcd12ca3714bf8f776adc7f08379cb7
codewithchinekwe <codewithchinekwe@gmail.com>
2023-02-17 19:22:12 +0100

    else statement

--- goControl/ifElse.go
+++ goControl/ifElse.go
@@ -1,26 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	/* local variable definition */
 	var a int = 10
-	// check the boolean condition using if statement
-
-	/*
-		Go If
-		The if statement in Go is used to test the condition. If it evaluates to true, the body of the statement is executed. If it evaluates to false, if block is skipped.
-
-		Syntax :
-
-		if(boolean_expression) {
-
-			   // statement(s) got executed only if the expression results in true
-			}
-	*/
+	/* check the boolean condition */
 	if a%2 == 0 {
-		fmt.Println(a, "is an even number")
+		/* if condition is true then print the following */
+		fmt.Printf("a is even\n")
+	} else {
+		/* if condition is false then print the following */
+		fmt.Printf("a is odd\n")
 	}
+	fmt.Printf("value of a is : %d\n", a)
 }
