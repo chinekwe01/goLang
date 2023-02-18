5209628dd28d7f5b97b3cac328e1512a31e8196a
codewithchinekwe <codewithchinekwe@gmail.com>
2023-02-18 05:05:15 +0100

    nested if statement

--- goControl/ifElse.go
+++ goControl/ifElse.go
@@ -3,22 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter text: ")
-	var input int
-	fmt.Scanln(&input)
-	if input < 0 || input > 100 {
-		fmt.Print("Please enter valid no")
-	} else if input >= 0 && input < 50 {
-		fmt.Print(" Fail\n")
-	} else if input >= 50 && input < 60 {
-		fmt.Print(" D Grade\n")
-	} else if input >= 60 && input < 70 {
-		fmt.Print(" C Grade\n")
-	} else if input >= 70 && input < 80 {
-		fmt.Print(" B Grade\n")
-	} else if input >= 80 && input < 90 {
-		fmt.Print(" A Grade\n")
-	} else if input >= 90 && input <= 100 {
-		fmt.Print(" A+ Grade\n")
+	/* local variable definition */
+	var x int = 10
+	var y int = 20
+	/* check the boolean condition */
+	if x >= 10 {
+		/* if condition is true then check the following */
+		if y >= 10 {
+			/* if condition is true then print the following */
+			fmt.Printf("Inside nested If Statement \n")
+		}
 	}
+	fmt.Printf("Value of x is : %d\n", x)
+	fmt.Printf("Value of y is : %d\n", y)
 }
