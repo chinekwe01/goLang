014bd149e5bb318360361f69951e0d3841ae857d
codewithchinekwe <codewithchinekwe@gmail.com>
2023-02-18 05:03:08 +0100

    if else-if chain

--- goControl/ifElse.go
+++ goControl/ifElse.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter number: ")
+	fmt.Print("Enter text: ")
 	var input int
 	fmt.Scanln(&input)
-	fmt.Print(input)
-	/* check the boolean condition */
-	if input%2 == 0 {
-		/* if condition is true then print the following */
-		fmt.Printf(" is even\n")
-	} else {
-		/* if condition is false then print the following */
-		fmt.Printf(" is odd\n")
+	if input < 0 || input > 100 {
+		fmt.Print("Please enter valid no")
+	} else if input >= 0 && input < 50 {
+		fmt.Print(" Fail\n")
+	} else if input >= 50 && input < 60 {
+		fmt.Print(" D Grade\n")
+	} else if input >= 60 && input < 70 {
+		fmt.Print(" C Grade\n")
+	} else if input >= 70 && input < 80 {
+		fmt.Print(" B Grade\n")
+	} else if input >= 80 && input < 90 {
+		fmt.Print(" A Grade\n")
+	} else if input >= 90 && input <= 100 {
+		fmt.Print(" A+ Grade\n")
 	}
 }
