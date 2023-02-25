5855b72048a6af54a34405f102e60e245c2f60eb
codewithchinekwe <codewithchinekwe@gmail.com>
2023-02-25 05:48:35 +0100

    continue statement with inner loop

--- goControl/continue.go
+++ goControl/continue.go
@@ -1,3 +1,22 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	/* local variable definition */
+// 	var a int = 1
+// 	/* do loop execution */
+// 	for a < 10 {
+// 		if a == 5 {
+// 			/* skip the iteration */
+// 			a = a + 1
+// 			continue
+// 		}
+// 		fmt.Printf("value of a: %d\n", a)
+// 		a++
+// 	}
+// }
+
 package main
 
 import "fmt"
@@ -5,14 +24,16 @@ import "fmt"
 func main() {
 	/* local variable definition */
 	var a int = 1
+	var b int = 1
 	/* do loop execution */
-	for a < 10 {
-		if a == 5 {
-			/* skip the iteration */
-			a = a + 1
-			continue
+	for a = 1; a < 3; a++ {
+		for b = 1; b < 3; b++ {
+			if a == 2 && b == 2 {
+				/* skip the iteration */
+				continue
+			}
+			fmt.Printf("value of a and b is %d %d\n", a, b)
 		}
-		fmt.Printf("value of a: %d\n", a)
-		a++
+		fmt.Printf("value of a and b is %d %d\n", a, b)
 	}
 }
