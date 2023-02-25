f40ea418aae353d2dac26fef1971568d00a6f1af
codewithchinekwe <codewithchinekwe@gmail.com>
2023-02-25 05:38:40 +0100

    break statement with inner loop

--- goControl/break.go
+++ goControl/break.go
@@ -1,15 +1,32 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a int = 1
+// 	for a < 10 {
+// 		fmt.Print("Value of a is ", a, "\n")
+// 		a++
+// 		if a > 5 {
+// 			/* terminate the loop using break statement */
+// 			break
+// 		}
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var a int = 1
-	for a < 10 {
-		fmt.Print("Value of a is ", a, "\n")
-		a++
-		if a > 5 {
-			/* terminate the loop using break statement */
-			break
+	var a int
+	var b int
+	for a = 1; a <= 3; a++ {
+		for b = 1; b <= 3; b++ {
+			if a == 2 && b == 2 {
+				break
+			}
+			fmt.Print(a, " ", b, "\n")
 		}
 	}
 }
